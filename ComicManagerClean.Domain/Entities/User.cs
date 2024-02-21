64d114e4408295b49f4a93a2c6302c7f87ed37b2
jrg96 <gomezlopez.jorge96@gmail.com>
2024-02-21 11:57:40 -0800

    Added User Entity

---
+++ ComicManagerClean.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComicManagerClean.Domain.Entities;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [StringLength(50)]
+    public string Name { get; set; }
+
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
