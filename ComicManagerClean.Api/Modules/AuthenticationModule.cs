d2cc4f33c158b15a252c4d806e2306f39276bf59
jrg96 <gomezlopez.jorge96@gmail.com>
2024-02-21 21:54:14 -0800

    Auth Register endpoint: Handle command failure scenarios

--- ComicManagerClean.Api/Modules/AuthenticationModule.cs
+++ ComicManagerClean.Api/Modules/AuthenticationModule.cs
@@ -5,6 +5,7 @@ using ComicManagerClean.Application.User.Commands;
 using ComicManagerClean.Contracts.Authentication;
 using ComicManagerClean.Contracts.Common;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComicManagerClean.Api.Modules;
 
@@ -23,6 +24,7 @@ public class AuthenticationModule : CarterModule
         v1.MapPost("/register", Register)
             .Produces<TaskResult<string>>(200)
             .Produces<TaskResult<string>>(500)
+            .Produces<TaskResult<string>>(400)
             .Accepts<SignUpRequest>("application/json");
     }
 
@@ -43,7 +45,7 @@ public class AuthenticationModule : CarterModule
     /*
      * Endpoint functions
      */
-    public async Task<TaskResult<string>> Register(IMediator mediator, SignUpRequest request)
+    public async Task<IResult> Register(IMediator mediator, SignUpRequest request)
     {
         var result = await mediator.Send(new CreateUserCommand(
             request.FirstName,
@@ -52,11 +54,21 @@ public class AuthenticationModule : CarterModule
             request.Password
         ));
 
-        return new TaskResult<string>()
+        if (!result.IsSuccess)
+        {
+            return TypedResults.BadRequest(new TaskResult<string>()
+            {
+                Successful = false,
+                ErrorList = new List<string>() { result.Error.Message },
+                Data = string.Empty
+            });
+        }
+
+        return TypedResults.Ok(new TaskResult<string>()
         {
             ErrorList = new List<string>(),
             Successful = result.IsSuccess,
-            Data = "Hello World!"
-        };
+            Data = "User registered successfully!"
+        });
     }
 }
