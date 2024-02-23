65b0931336f3213da1e27927277e84db2d5210d5
jrg96 <gomezlopez.jorge96@gmail.com>
2024-02-22 23:52:20 -0800

    minimal API implementation for Login action

--- ComicManagerClean.Api/Modules/AuthenticationModule.cs
+++ ComicManagerClean.Api/Modules/AuthenticationModule.cs
@@ -5,7 +5,6 @@ using ComicManagerClean.Application.User.Commands;
 using ComicManagerClean.Contracts.Authentication;
 using ComicManagerClean.Contracts.Common;
 using MediatR;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ComicManagerClean.Api.Modules;
 
@@ -22,10 +21,16 @@ public class AuthenticationModule : CarterModule
         var v1 = authGroup.MapGroup("").HasApiVersion(1);
 
         v1.MapPost("/register", Register)
+            .Accepts<SignUpRequest>("application/json")
             .Produces<TaskResult<string>>(200)
             .Produces<TaskResult<string>>(500)
-            .Produces<TaskResult<string>>(400)
-            .Accepts<SignUpRequest>("application/json");
+            .Produces<TaskResult<string>>(400);
+
+        v1.MapPost("/login", Login)
+            .Accepts<AuthenticationRequest>("application/json")
+            .Produces<TaskResult<string>>(200)
+            .Produces<TaskResult<string>>(500)
+            .Produces<TaskResult<string>>(400);
     }
 
     private RouteGroupBuilder CreateApiVersions(IEndpointRouteBuilder app)
@@ -69,6 +74,31 @@ public class AuthenticationModule : CarterModule
             ErrorList = new List<string>(),
             Successful = result.IsSuccess,
             Data = "User registered successfully!"
+        });
+    }
+
+    public async Task<IResult> Login(IMediator mediator, AuthenticationRequest request)
+    {
+        var result = await mediator.Send(new LoginUserCommand(
+            request.Email,
+            request.Password
+        ));
+
+        // If there was a failure, return Bad Request
+        if (!result.IsSuccess)
+        {
+            return TypedResults.BadRequest(new TaskResult<string>() {
+                Successful = false,
+                ErrorList = new List<string>() { result.Error.Message },
+                Data = string.Empty
+            });
+        }
+
+        return TypedResults.Ok(new TaskResult<string>()
+        {
+            ErrorList = new List<string>(),
+            Successful = true,
+            Data = "YOUR_JSON_WEB_TOKEN"
         });
     }
 }
