692af54151c9395e2e16048fa6df730303777531
jrg96 <gomezlopez.jorge96@gmail.com>
2024-02-22 20:10:56 -0800

    Added validations for CreateUserCommand

---
+++ ComicManagerClean.Application/User/Validators/CreateUserCommandValidator.cs
@@ -0,0 +1,43 @@
+﻿using ComicManagerClean.Application.User.Commands;
+using FluentValidation;
+using System.Text.RegularExpressions;
+
+namespace ComicManagerClean.Application.User.Validators;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(20)
+            .Must(IsValidPassword);
+
+        RuleFor(command => command.Name)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(command => command.LastName)
+            .NotEmpty()
+            .NotNull();
+            
+    }
+
+    private bool IsValidPassword(string password)
+    {
+        var lowercase = new Regex("[a-z]+");
+        var uppercase = new Regex("[A-Z]+");
+        var digit = new Regex("(\\d)+");
+        var symbol = new Regex("(\\W)+");
+
+        return (lowercase.IsMatch(password) 
+            && uppercase.IsMatch(password) 
+            && digit.IsMatch(password) 
+            && symbol.IsMatch(password));
+    }
+}
