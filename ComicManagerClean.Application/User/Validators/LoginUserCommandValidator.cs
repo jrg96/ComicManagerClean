64766ef4eff1f551847015fbe052a2a05877f1b2
jrg96 <gomezlopez.jorge96@gmail.com>
2024-02-22 23:56:06 -0800

    Added validator for LoginUserCommand

---
+++ ComicManagerClean.Application/User/Validators/LoginUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using ComicManagerClean.Application.User.Commands;
+using FluentValidation;
+
+namespace ComicManagerClean.Application.User.Validators;
+
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserCommandValidator()
+    {
+        RuleFor(login => login.Email)
+            .NotEmpty()
+            .NotNull();
+
+        RuleFor(login => login.Password)
+            .NotEmpty()
+            .NotNull();
+    }
+}
